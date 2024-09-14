86726a5fc1464d2a8340f1d6a74caf77ea6e5762
2001870 <3183396080@qq.com>
2024-09-14 17:01:46 +0800

    init

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time  # 用于模拟延时
 
-st.set_page_config(page_title='lover', page_icon="💌")
+st.set_page_config(page_title='lover')
 
 valid_names = ['唐悦媛', '葛青怡', '张雨萌', '居仕琪', '杨万兴', '赵吕聪',
                '刘莘', '周诣扬', '李瑞', '许志奥', '谷雨凡', '王其兰',
@@ -14,8 +14,8 @@ if st.session_state.page == 'main':
     st.title('姓名输入')
     name = st.text_input("请输入您的姓名")
 
-    if st.button('💞'):
-        with st.spinner('🎑正在验证姓名...'):
+    if st.button('开始'):
+        with st.spinner('正在验证姓名...'):
             time.sleep(1)
             if name in valid_names:
                 st.session_state.page = 'holiday'
@@ -23,8 +23,8 @@ if st.session_state.page == 'main':
                 st.warning("姓名不在允许的列表中，请输入有效的姓名")
 
 elif st.session_state.page == 'holiday':
-    st.markdown("<h1 style='text-align: center;'>中秋快乐🤗!!!</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>中秋快乐!!!</h1>", unsafe_allow_html=True)
 
-    if st.button('🥮 返回首页  🌕', key='return_button', help='点击返回首页'):
+    if st.button('返回首页', key='return_button', help='点击返回首页'):
         st.session_state.page = 'main'
 
